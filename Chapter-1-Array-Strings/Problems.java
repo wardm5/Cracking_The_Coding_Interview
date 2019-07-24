908b5d51078c42af0690c4fa52b5421bd5a2ddd9
Misha Ward <wardm5@uw.edu>
2019-07-24 11:51:35 -0700

    updated first problem isUnique, will work on when there is no data structures allowed

--- Chapter-1-Array-Strings/Problems.java
+++ Chapter-1-Array-Strings/Problems.java
@@ -1,6 +1,19 @@
+import java.util.*;
 public class Problems {
     public void test() {
         System.out.println("This test works! ");
     }
-
+    // if allowed additional datastructures such as Set
+    public boolean isUnique1(String str) {
+        HashSet<Character> set = new HashSet<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (set.contains(str.charAt(i)))
+                return false;
+            set.add(str.charAt(i));
+        }
+        return true;
+    }
+    // public isUnique2() {
+    //
+    // }
 }
--- Chapter-1-Array-Strings/main.java
+++ Chapter-1-Array-Strings/main.java
@@ -1,6 +1,7 @@
 public class main {
     public static void main(String[] args) {
         Problems problems = new Problems();
-        
+        System.out.println(problems.isUnique1("abc"));  // return true
+        System.out.println(problems.isUnique1("abbc")); // return false
     }
 }
