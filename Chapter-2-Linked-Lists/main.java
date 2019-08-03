8997d727c30b6e4c5be95180a94e05433419d3f5
Misha Ward <wardm5@uw.edu>
2019-08-03 00:19:54 -0700

    updating file to include removeDuplicates2, for future work

--- Chapter-2-Linked-Lists/main.java
+++ Chapter-2-Linked-Lists/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args) {
         Problems problems = new Problems();
-        problems.test();
+        // LinkedList setup
         LinkedList<Integer> list = new LinkedList<>();
         list.addToTail(1);
         list.addToTail(2);
@@ -10,10 +10,16 @@ public class main {
         list.addToTail(2);
         list.addToTail(3);
 
-        // 1.1 removeDuplicates - data structures allowed...
+        // 1.1 removeDuplicates1 - data structures allowed...
         System.out.println("\n1.1 removeDuplicates1");
-        problems.removeDuplicates1(list);
+        problems.removeDuplicates1(list);   // takes O(n) time due to going through entire list once.
         list.print();
+
+        // 1.1 removeDuplicates2 - no data structures allowed...
+        System.out.println("\n1.1 removeDuplicates2");
+        problems.removeDuplicates1(list);   // takes O(n) time due to going through entire list once.
+        list.print();
+
 
     }
     private static void write(String input) {
